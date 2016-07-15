bcb412aab957caed2a2fcc0d7f5350eda753a286
Montmorency <lamberh@tcd.ie>
2016-07-15 18:11:16 +0100

    breaking tight binding

--- simulate_crack.py
+++ simulate_crack.py
@@ -29,18 +29,20 @@ cutoff_skin = 2.0*units.Ang    # Amount by which potential cutoff is increased
                                # for neighbour calculations
 tip_move_tol = 10.0            # Distance tip has to move before crack
                                # is taken to be running
-strain_rate    = 1.e-7*(1.0/(units.fs))
+#Need have simulate crack use the same crack dictionary so I don't need to do this by hand.
+#strain_rate = 1e-5*(1.0/units.fs) 
+strain_rate    = 1.e-5*(1.0/(7.93*units.fs))
 traj_interval  = 10             # Number of time steps between
 traj_file      = 'traj_lotf_2.xyz'    # Trajectory output file in (NetCDF format)
-restart_traj_file = 'traj_lotf_2b.xyz'    # Trajectory output file in (NetCDF format)
-print_interval    = 10            # time steps between trajectory prints 10 fs
-param_file = 'Fe_Mendelev.xml'      # Filename of XML file containing
+restart_traj_file      = 'traj_lotf_2b.xyz'    # Trajectory output file in (NetCDF format)
+print_interval = 10            # time steps between trajectory prints 10 fs
+param_file = 'params.xml'      # Filename of XML file containing
                                # potential parameters
-mm_init_args = 'IP EAM_ErcolAd'         # Classical potential
+mm_init_args = 'IP SW'         # Classical potential
 set_fortran_indexing(False)
 
-sw_pot = Potential('IP EAM_ErcolAd', param_filename=param_file, cutoff_skin=cutoff_skin)
-qm_pot = Potential(qm_init_args, param_filename=param_file)
+#sw_pot = Potential('IP SW', param_filename='params.xml', cutoff_skin=cutoff_skin)
+#qm_pot = Potential(qm_init_args, param_filename='params.xml')
 restart = True
 #if from scratch we load the original cell:
 #atoms = AtomsReader('crack.xyz')
@@ -54,7 +56,7 @@ def fix_edges(atoms):
   left   = atoms.positions[:,0].min()
   right  = atoms.positions[:,0].max()
   fixed_mask = ((abs(atoms.positions[:, 1] - top) < 2.0) |
-                (abs(atoms.positions[:, 1] - bottom) < 2.0))
+               (abs(atoms.positions[:, 1] - bottom) < 2.0))
   fix_atoms  = FixAtoms(mask=fixed_mask)
   print('Fixed %d atoms\n' % fixed_mask.sum()) # Print the number of fixed atoms
   strain_atoms = ConstantStrainRate(orig_height, strain_rate*timestep)
@@ -62,27 +64,36 @@ def fix_edges(atoms):
   return strain_atoms
 
 def pass_print_context(atoms, dynamics):
-	def printstatus():
-		if (dynamics.nsteps%20)==0:
-			print """
+  def printstatus():
+    #if dynamics.nsteps == 1:
+    if (dynamics.nsteps%10)==0:
+      print """
 State      Time/fs    Temp/K     Strain      G/(J/m^2)  CrackPos/A D(CrackPos)/A 
 ---------------------------------------------------------------------------------"""
-			log_format = ('%(label)-4s%(time)12.1f%(temperature)12.6f'+
-			              '%(strain)12.5f%(G)12.4f%(crack_pos_x)12.2f    (%(d_crack_pos_x)+5.2f)')
-			
-			orig_crack_pos = atoms.info['CrackPos'].copy()
-			#atoms.info['label'] = dynamics.state_label                # Label for the status line
-			atoms.info['label'] = 'EAM'                # Label for the status line
-			atoms.info['time'] = dynamics.get_time()/units.fs
-			atoms.info['temperature'] = (atoms.get_kinetic_energy() /
-			                             (1.5*units.kB*len(atoms)))
-			atoms.info['strain'] = get_strain(atoms)
-			atoms.info['G'] = get_energy_release_rate(atoms)/(units.J/units.m**2)
-			crack_pos = find_crack_tip_stress_field(atoms, calc=sw_pot)
-			atoms.info['crack_pos_x'] = crack_pos[0]
-			atoms.info['d_crack_pos_x'] = crack_pos[0] - orig_crack_pos[0]
-			print log_format % atoms.info
-	return printstatus
+      log_format = ('%(label)-4s%(time)12.1f%(temperature)12.6f'+
+                    '%(strain)12.5f%(G)12.4f%(crack_pos_x)12.2f    (%(d_crack_pos_x)+5.2f)')
+      log_format2 = ('%(label)-4s%(time)12.1f%(temperature)12.6f'+
+                    '%(strain)12.5f%(G)12.4f)')
+      try:
+        atoms.info['label'] = dynamics.state_label                # Label for the status line
+      except AttributeError:
+        atoms.info['label'] = 'classical'                # Label for the status line
+        
+
+      atoms.info['time']  = dynamics.get_time()/units.fs
+      atoms.info['temperature'] = (atoms.get_kinetic_energy() /
+                                   (1.5*units.kB*len(atoms)))
+      atoms.info['strain'] = get_strain(atoms)
+      atoms.info['G']      = get_energy_release_rate(atoms)/(units.J/units.m**2)
+      try:
+        orig_crack_pos = atoms.info['CrackPos'].copy()
+        crack_pos = find_crack_tip_stress_field(atoms, calc=sw_pot)
+        atoms.info['crack_pos_x']   = crack_pos[0]
+        atoms.info['d_crack_pos_x'] = crack_pos[0] - orig_crack_pos[0]
+        print log_format % atoms.info
+      except KeyError:
+        print log_format2 % atoms.info
+  return printstatus
 
 #pot1 is the low precision
 #pot2 is the high precision, i.e. QM potential
