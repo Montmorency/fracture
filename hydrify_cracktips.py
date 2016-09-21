f718474223b54e55ac279c3fd9443115bba118b5
Montmorency <lamberh@tcd.ie>
2016-09-21 18:15:10 +0100

    big update to add hydrogen atoms. Now using voronoi tessellation to calculate favourable positions to place hydrogen.

--- hydrify_cracktips.py
+++ hydrify_cracktips.py
@@ -1,27 +1,76 @@
 import os
 import glob 
 import shutil
-from quippy import Atoms
+import argparse 
+import numpy as np
+import scipy.spatial as spatial
 
-jobs = glob.glob('T*z1')
+from   quippy import Atoms
 
-# Create new directories which mimic the original
-# pristine crack cell but then add hydrogen at the
-# crack tip.
-for job in jobs:
-  os.mkdir(job+'H1')
-  input  = os.path.join(job, 'crack.xyz')
-  pickle = os.path.join(job, 'crack_info.pckl')
-  shutil.copy(input, job+'H1')
-  shutil.copy(pickle, job+'H1')
+class Hydrify(object):
+  """"
+  class:Hydrify contains methods for adding hydrogens at specific positions in a 
+  simulation cell.
+  """ 
+  def hydrify_multi_dirs(self, jobs, crack_pos):
+    """
+    Create new directories which mimic the original
+    pristine crack cell but then add hydrogen at the
+    crack tip. 
+    """
+    for job in jobs:
+      os.mkdir(job+'H1')
+      input  = os.path.join(job, 'crack.xyz')
+      pickle = os.path.join(job, 'crack_info.pckl')
+      shutil.copy(input, job+'H1')
+      shutil.copy(pickle, job+'H1')
+    for job in jobs:
+      os.chdir(job)
+      print job
+      cr = Atoms('crack.xyz')
+      self.hydrify_single_job(cr)
+      cr.add_atoms([109.17,-1.11,4.275],1)
+      cr.set_atoms(cr.numbers)
+      cr.write('crack.xyz')
+      os.chdir('../')
 
-jobs = glob.glob('T*H1')
-for job in jobs:
-  os.chdir(job)
-  print job
-  cr = Atoms('crack.xyz')
-  cr.add_atoms([109.17,-1.11,4.275],1)
-  cr.set_atoms(cr.numbers)
-  cr.write('crack.xyz')
-  os.chdir('../')
+  def hydrify_single_job(self, ats, positions=[]):
+    """
+    Add hydrogens at positions in list.
+    """
+    for h_pos in positions:
+      ats.add_atoms([109.17,-1.11,4.275],1)
+      ats.set_atoms(cr.numbers)
+
+  def add_h_smart(self, ats, position=np.array([0.0,0.0,0.0]),rr=7.0,n_h=1):
+    """
+    Given a position try to add the hydrogen in a "nice"
+    place. rr is radius of clust
+    """
+# First take a small selection of atoms around the crack tip
+    ats.calc_connect(2.5)
+    fixed_mask = (np.sqrt(sum(map(np.square,(ats.positions[:,0:3]-ats.params['CrackPos'][:])))) < rr)
+    cl         = ats.select(fixed_mask)
+#  Then compute voronoi diagram and add hydrogens around there:
+    vor        = spatial.Voronoi(cl.positions, furthest_site=True)
+    for i in range(n_h):
+      ats.add_atoms(vor.vertices[i], 1)
+
+
+if __name__=='__main__':
+  parser  = argparse.ArgumentParser()
+  parser.add_argument('-p','--pattern', default="nopattern")
+  args    = parser.parse_args()
+  pattern = args.pattern
+
+  jobs = glob.glob(pattern+"*xyz")
+  hydrify =  Hydrify()
+
+  for job in jobs:
+    ats = Atoms(job)
+    print job
+    print len(ats)
+    print ats.params['CrackPos']
+    hydrify.add_h_smart(ats, position=[ats.params['CrackPos']])
+    ats.write('thermalizedH.xyz')
 
--- tb_pot.py
+++ tb_pot.py
@@ -11,33 +11,35 @@ class TightBindingPot(object):
   """
   Interface to the lmto-tb code. Contains the input
   file and a routine for writing the ctrl.{atom} file.
+  relax= 5 for FLETCHER/G
   """
-  def __init__(self, alat=2.83, nk=4, n=4):
-    self.alat = alat # lattice constant, angstrom
-    self.nk   = nk   # number of k-points along cubic axes
-    self.n    = n    # we use an n x n x n supercell of primitive cell
+  def __init__(self, alat=1.00, nk=1, n_sup=1):
+    self.alat = alat   # lattice constant, angstrom
+    self.nk   = nk     # number of k-points along cubic axes
+    self.n    = n_sup  # we use an n x n x n supercell of primitive cell
+#We've removed carbon from this input file.
     self.template = """HEADER auto-generated control file
 %% const fp=0 cpl=1 xcf=4 gga=3
 %% const pureFe=0 PRBmodel=0 CModel2=0 scale=1.0
 %% const epsilon=1 sign=epsilon?-1:1 xFe=1/3
-%% const nbas=%(NBAS)d nspec=3
-%% const sd=1 ovlp=sd ul=1 u1=0 tbu=0 io=1 nitq=50
+%% const nbas=%(NBAS)d nspec=%(NSPEC)d
+%% const sd=1 ovlp=sd ul=1 u1=0 tbu=0 io=0 nitq=50
 %% const verb=31 so=0 nsp=2 tetra=0 metal={fp?3:1} width=0.002 N=1
 %% const au=0.529177 NdFe=6.8
-%% const beta=0.2 nx=5 kmix=300 nav=0
-%% const show=0 mpol=0
+%% const beta=0.06 kmix=300 nav=0
+%% const show=0 mpol=0 nx=5
 %% const dyn=0 temp=300 taup=10 taub=100 time=100000 tstep=5
-%% const hess=F relax=0 nit=50 xtol=1d-3 gtol=5d-4 step=0.01 nkill=100 nitf=50
+%% const hess=F relax=0 nit=60 xtol=1d-3 gtol=5d-4 step=0.01 nkill=100 nitf=50
 %% const fs=0.048377 K=1/0.6333328d-5 amass=1.09716d-3
 VERS    TB=10 LM=7 FP=7 ASA=7
 IO      SHOW={show} HELP=F VERBOS={verb} WKP=F
-CONST   nit=100 conv=1d-2 qtol=1d-2 pair=F
+CONST   nit=100 conv=1d-4 qtol=1d-3 pair=F
         V0at=79.76508 V0bcc=V0at*2 V0fcc=V0at*4
         vfrac0=1 vfrac=vfrac0 Vbcc=vfrac*V0bcc Vfcc=vfrac*V0fcc
         q0=sqrt(8/3) q=q0 qJang=1.57693 qexp=1.582 qTB=1.614
         ahcp=(Vfcc/(sqrt(3)*q))^(1/3)        
         abcc=Vbcc^(1/3) afcc=Vfcc^(1/3) aeps=sqrt(3)*ahcp
-        alat=%(ALAT)f
+        alat=%(ALAT)f 
         nk=%(NK)d mull=-1 bzj=1 ewtol=1d-6 ef0=0.643
         R=2.2 RC=0.8
 %% const NdFe=7 V0at=79.76508 V0bcc=V0at*2 a0bcc=V0bcc^(1/3) 
@@ -48,19 +50,8 @@ CONST   nit=100 conv=1d-2 qtol=1d-2 pair=F
 %% else
         fixscale=a0bcc
 %% endif
-        r1CFs0=0.5278607028 rcCFs0=1.789982983
-        r1CFsp0=0.6109702668 rcCFsp0=1.643917618
-        r1CFd0=0.5945948286 rcCFd0=1.673566269
-        r1CFsau=r1CFs0*fixscale rcCFsau=rcCFs0*fixscale 
-        r1CFspau=r1CFs0*fixscale rcCFspau=rcCFs0*fixscale 
-        r1CFdau=r1CFd0*fixscale rcCFdau=rcCFd0*fixscale 
-        r1CFpp0=0.5007117092 rcCFpp0=1.507038147 
-        r1CFppau=r1CFpp0*fixscale rcCFppau=rcCFpp0*fixscale
         r1HFau=0.8*fixscale rcHFau=2*fixscale
         r1HFppau=0.75*fixscale rcHFppau=0.95*fixscale
-        r1CC0=0.6 rcCC0=1
-        CCrc1au=3.6 CCrc2au=5.42
-        r1CCau=r1CC0*fixscale rcCCau=rcCC0*fixscale
         r1ssau=1.1*fixscale rcssau=2*fixscale 
         r1sdau=1.1*fixscale rcsdau=2*fixscale 
         r1ddau={PRBModel?1.1:0.9}*fixscale rcddau=1.4*fixscale 
@@ -107,42 +98,6 @@ CONST   nit=100 conv=1d-2 qtol=1d-2 pair=F
         qoss=qoss0 qosd=qosd0
         oss=ossr0*exp(qoss*r0ff)
         osd=osdr0*exp(qosd*r0ff)
-# Fe-C
-        r0CF=3.519361994
-        qCFss0=0.6 qCFsp0=0.6 qCFsd0=0.6 qCFpds0=0.7 qCFpdp0=0.7
-        fCFss0=-2 fCFsp0=2.25 fCFsd0=-0.5
-        fCFpds0=-1.5 fCFpdp0=1
-        hCFssr0=fCFss0*exp(-qCFss0*r0CF)
-        hCFspr0=fCFsp0*exp(-qCFsp0*r0CF)
-        hCFsdr0=fCFsd0*exp(-qCFsd0*r0CF)
-        hpdsr0=fCFpds0*exp(-qCFpds0*r0CF)
-        hpdpr0=fCFpdp0*exp(-qCFpdp0*r0CF)
-        qCFss=0.5654777585 qCFsp=0.7602419272 qCFsd=0.3024914302
-        qCFpds=0.6436211918 qCFpdp=0.6652876311 
-        fCFss=hCFssr0*exp(qCFss*r0CF)
-        fCFsp=hCFspr0*exp(qCFsp*r0CF)
-        fCFsd=hCFsdr0*exp(qCFsd*r0CF)
-        fCFpds=hpdsr0*exp(qCFpds*r0CF)
-        fCFpdp=hpdpr0*exp(qCFpdp*r0CF)
-        ofacCFss=0.5502992445 ofacCFsp=0.5487607608
-        ofacCFsd=0.3601562852 ofacCFpd=0.4335108427
-        qoCFss0=0.6 qoCFsp0=0.6 qoCFsd0=0.5 
-        qoCFpds0=0.5 qoCFpdp0=0.5
-        oCFss0=-ofacCFss*fCFss0 oCFsp0=-ofacCFsp*fCFsp0 
-        oCFsd0=-ofacCFsd*fCFsd0
-        oCFpds0=-ofacCFpd*fCFpds0 oCFpdp0=-ofacCFpd*fCFpdp0
-        oCFssr0=oCFss0*exp(-qoCFss0*r0CF)
-        oCFspr0=oCFsp0*exp(-qoCFsp0*r0CF)
-        oCFsdr0=oCFsd0*exp(-qoCFsd0*r0CF)
-        opdsr0=oCFpds0*exp(-qoCFpds0*r0CF)
-        opdpr0=oCFpdp0*exp(-qoCFpdp0*r0CF)
-        qoCFss=0.3010599981 qoCFsp=0.3911389194 qoCFsd=0.3408022068
-        qoCFpds=0.3063617442 qoCFpdp=0.4551807593
-        oCFss=oCFssr0*exp(qoCFss*r0CF)
-        oCFsp=oCFspr0*exp(qoCFsp*r0CF)
-        oCFsd=oCFsdr0*exp(qoCFsd*r0CF)
-        oCFpds=opdsr0*exp(qoCFpds*r0CF)
-        oCFpdp=opdpr0*exp(qoCFpdp*r0CF)
 # Fe-H
         r0HF=1.453500953
         qHFss0=0.592 qHFsd0=0.601
@@ -165,30 +120,6 @@ CONST   nit=100 conv=1d-2 qtol=1d-2 pair=F
         fHHss=0 qHHss=0.5 
         fHFsp=0 qHFsp=0
         oHHss=0 oHFsp=0 qoHFsp=0
-# C-C
-%% const Ry=13.61 au=0.529177 d0d=1.54/au
-# Harrison translated to exponential scaling
-#        vsss=-5/{Ry}*exp(2) vsps=4.7/{Ry}*exp(2) 
-#        vpps=5.5/{Ry}*exp(2) vppp=-1.55/{Ry}*exp(2)
-#        decayCC=2/d0d mCC=0 pCC=2*decayCC bCC=38 CCmode=2
-# Harrison's power law (Xu, Wang, Chan and Ho, JPCM 4, 6047 (1992))
-        vsss=-5/{Ry}*{d0d}^2 vsps=4.7/{Ry}*{d0d}^2 
-        vpps=5.5/{Ry}*{d0d}^2 vppp=-1.55/{Ry}*{d0d}^2
-        decayCC=2 mCC=-4 pCC=0 bCC=43 CCmode=3
-        qssCC=decayCC qspCC=decayCC qppCC=decayCC
-        CCscal=1 oCCscal=0
-        fCCsss=CCscal*vsss fCCsps=CCscal*vsps
-        fCCpps=CCscal*vpps fCCppp=CCscal*vppp
-        oCCsss=-oCCscal*vsss oCCsps=-oCCscal*vsps
-        oCCpps=-oCCscal*vpps oCCppp=-oCCscal*vppp
-# Terence C-C model (GSP)
-#        CCmode=5
-#        CCsss=-0.37241 CCsps=0.481098 CCpps=0.32075 CCppp=-0.06013
-#        CCnsss=2.95401 CCnsps=2.92818 CCnpps=2.93431 CCnppp=2.92822
-#        CCnc=6.5 CCr0=2.90319 CCrc=4.11960
-#        CCA=1.15575
-#        CCnp=3.69592 
-#        CCncp=5.96232 CCr0p=CCr0 CCrcp=4.1950
 # Fe-Fe pair potential
 %% ifdef sd
 %%  ifdef scale
@@ -207,34 +138,19 @@ CONST   nit=100 conv=1d-2 qtol=1d-2 pair=F
          b0=683.1 m0=0 p0=1.5376 b1=-459.5 m1=0 p1=1.4544
 %%  endif
 %% endif
-# Fe-C pair potential
-        q0CF=2.396165226 n0CF=0 b0CFfac=0.7711879106 
-        q1CF=1.555534479  n1CF=0 b1CFfac=-0.01932497471
-        b0CF0=1000 b0CF=b0CF0*b0CFfac
-        b1CF0=1000 b1CF=b1CF0*b1CFfac
 # Fe-H pair potential
         qHF=2.69224661 nHF=-1 bHFfac=0.2995633136
         bHF0=1000 bHF=bHF0*bHFfac
-# C-C pair potential
-# see C-C hopping above
-# cut-offs in alat units
 %% ifdef scale
         ascale = alat
 %% else
         ascale = 1
 %% endif
         rmaxh=rmaxhau/ascale
-        r1CFs=r1CFsau/ascale rcCFs=rcCFsau/ascale 
-        r1CFsp=r1CFsau/ascale rcCFsp=rcCFsau/ascale 
-        r1CFd=r1CFdau/ascale rcCFd=rcCFdau/ascale 
-        r1CFpp=r1CFppau/ascale rcCFpp=rcCFppau/ascale
         r1HF=r1HFau/ascale rcHF=rcHFau/ascale
-        r1CC=r1CCau/ascale rcCC=rcCCau/ascale
         r1ss=r1ssau/ascale rcss=rcssau/ascale r1sd=r1sdau/ascale rcsd=rcsdau/ascale 
         r1dd=r1ddau/ascale rcdd=rcddau/ascale r1pp=r1ppau/ascale rcpp=rcppau/ascale
-        r1CFpp=r1CFppau/ascale rcCFpp=rcCFppau/ascale
         r1HFpp=r1HFppau/ascale rcHFpp=rcHFppau/ascale
-        CCrc1=CCrc1au/ascale CCrc2=CCrc2au/ascale
 %% ifdef mpol
         spp=0 ppd=0 sdd=0 pdp=1 ddd=3 pdf=0 ddg=6
 %% else
@@ -273,15 +189,6 @@ SPEC
 %%  else
         IDXDN={sd?1:3} 3 1 QPOL= spp ppd sdd pdp ddd pdf ddg 0 0 0
 %%  endif
-        ATOM=C Z=6 R=RC I=stniC A=0.025 AMASS=12.0107/{amass}
-        LMX=2 LMXL=2 LMXA=2
-        IDU= 0 0 0 0 UH= 0 0 0 0  JH=stniC stniC stniC stniC
-        COLOUR=0.5 0 0  RADIUS=0.25
-        RSMH=0.9 0.9 0.9 0 EH=-0.1 -0.1 -0.1 -0.1
-        MMOM=0 2 0
-%% ifndef fp
-        IDXDN=1 1 3 
-%% endif
         ATOM=H Z=1 R=RH I=stniH A=0.025 AMASS=1.00794/{amass}
         LMX=2 LMXL=2 LMXA=2
         IDU= 0 0 0 0 UH= 0 0 0 0  JH=stniH stniH stniH stniH
@@ -299,13 +206,6 @@ START   CNTROL=T
                      q0s/{nsp}            esFe   Us
                      q0p/{nsp}            epFe   Up
                      (q0dFe-momFe)/{nsp}  edFe  UdFe
-        ATOM=C    P= 1 2 3 1 2 3
-                  Q= q0sC/{nsp}           esC    UC
-                     q0pC/{nsp}           epC    UC
-                     0                    0      0
-                     q0sC/{nsp}           esC    UC
-                     q0pC/{nsp}           epC    UC
-                     0                    0      0
         ATOM=H    P= 1 2 3 1 2 3
                   Q= (q0H+momH)/{nsp}     esH    UH
                      0                    0      0
@@ -326,63 +226,6 @@ ME
         DECAY=qoss qsp qpp qpp    qosd qpd  qpd    qdds qddp qddd
         CUT=r1ss rcss 0 0 0 0 0 0 r1sd rcsd 0 0 0 0 r1dd rcdd r1dd rcdd r1dd rcdd
             ! b0 m0 p0   b1 m1 p1   0 0 0
-        C C MEMODE=CCmode PPMODE=10 POLY=5 CUTMOD=cutmod CUTPP=r1CC rcCC
-            | fCCsss fCCsps fCCpps fCCppp 0 0 0 0 0 0 
-        DECAY=qssCC   qspCC    qppCC    qppCC     0 0 0 0 0 0
-        CUT=  r1CC rcCC r1CC rcCC r1CC rcCC r1CC rcCC 
-              0 0  0 0  0 0  0 0  0 0  0 0  
-            @ oCCsss oCCsps oCCpps oCCppp 0 0 0 0 0 0
-        DECAY=qssCC   qspCC    qppCC    qppCC     0 0 0 0 0 0
-        CUT=  r1CC rcCC r1CC rcCC r1CC rcCC r1CC rcCC 
-              0 0  0 0  0 0  0 0  0 0  0 0  
-            ! bCC mCC pCC  0 0 0    0 0 0
-# Terence C-C model (GSP)
-#         2 2 MEMODE=CCmode PPMODE=30 POLY=5 CUTMOD=2 CUTPP=CCrc1 CCrc2   
-#               | CCsss CCnsss CCnc CCr0 CCrc 
-#                 CCsps CCnsps CCnc CCr0 CCrc 
-#                 CCpps CCnpps CCnc CCr0 CCrc 
-#                 CCppp CCnppp CCnc CCr0 CCrc  
-#                 0 0 0 1 1
-#                 0 0 0 1 1
-#                 0 0 0 1 1
-#                 0 0 0 1 1
-#                 0 0 0 1 1
-#                 0 0 0 1 1
-#               CUT= CCrc1 CCrc2 CCrc1 CCrc2 CCrc1 CCrc2 CCrc1 CCrc2
-#                    0 0 0 0 0 0 0 0 0 0 0 0
-#               @ 0 0 0 1 1
-#                 0 0 0 1 1
-#                 0 0 0 1 1
-#                 0 0 0 1 1
-#                 0 0 0 1 1
-#                 0 0 0 1 1
-#                 0 0 0 1 1
-#                 0 0 0 1 1
-#                 0 0 0 1 1
-#                 0 0 0 1 1
-#               CUT= CCrc1 CCrc2 CCrc1 CCrc2 CCrc1 CCrc2 CCrc1 CCrc2
-#                    0 0 0 0 0 0 0 0 0 0 0 0
-#               ! CCA 1 -1 CCnp CCncp CCr0p CCrcp 0 0  
-        Fe C MEMODE=2 PPMODE=10 POLY=5 CUTMOD=cutmod CUTPP=r1CFpp rcCFpp
-            | fCFss  fCFsp  0  0 fCFsd fCFpds fCFpdp 0 0 0
-        DECAY=qCFss  qCFsp  0  0 qCFsd qCFpds qCFpdp 0 0 0
-        CUT=  r1CFs rcCFs r1CFsp rcCFsp 0 0 0 0
-              r1CFd rcCFd r1CFd  rcCFd r1CFd rcCFd 0 0 0 0 0 0 
-            @ oCFss  oCFsp  0  0 oCFsd oCFpds oCFpdp 0 0 0
-        DECAY=qoCFss qoCFsp  0  0 qoCFsd qoCFpds qoCFpdp 0 0 0
-        CUT=  r1CFs rcCFs r1CFsp rcCFsp 0 0 0 0
-              r1CFd rcCFd r1CFd  rcCFd r1CFd rcCFd 0 0 0 0 0 0 
-            ! b0CF n0CF q0CF   b1CF n1CF q1CF  0 0 0
-        C Fe MEMODE=2 PPMODE=10 POLY=5 CUTMOD=cutmod CUTPP=r1CFpp rcCFpp
-            | fCFss  fCFsp  0  0 fCFsd fCFpds fCFpdp 0 0 0
-        DECAY=qCFss  qCFsp  0  0 qCFsd qCFpds qCFpdp 0 0 0
-        CUT=  r1CFs rcCFs r1CFsp rcCFsp 0 0 0 0
-              r1CFd rcCFd r1CFd  rcCFd r1CFd rcCFd 0 0 0 0 0 0 
-            @ oCFss  oCFsp  0  0 oCFsd oCFpds oCFpdp 0 0 0
-        DECAY=qoCFss qoCFsp  0  0 qoCFsd qoCFpds qoCFpdp 0 0 0
-        CUT=  r1CFs rcCFs r1CFsp rcCFsp 0 0 0 0
-              r1CFd rcCFd r1CFd  rcCFd r1CFd rcCFd 0 0 0 0 0 0 
-            ! b0CF n0CF q0CF   b1CF n1CF q1CF  0 0 0
         Fe H MEMODE=2 PPMODE=10 POLY=5 CUTMOD=cutmod CUTPP=r1HFpp rcHFpp
             | fHFss fHFsp  0  0 fHFsd 0 0 0 0 0
         DECAY=qHFss qHFsp  0  0 qHFsd 0 0 0 0 0
@@ -409,33 +252,13 @@ ME
         CUT=  0 0  0 0  0 0  0 0  0 0  0 0  
               0 0  0 0  0 0  0 0  0 0  0 0  
             ! 0 0 0  0 0 0    0 0 0
-        C H MEMODE=2 PPMODE=0
-            | 0 0 0 0 0 0 0 0 0 0 
-        DECAY=0 0 0 0 0 0 0 0 0 0
-        CUT=  0 0  0 0  0 0  0 0  0 0  0 0  
-              0 0  0 0  0 0  0 0  0 0  0 0  
-            @ 0 0 0 0 0 0 0 0 0 0
-        DECAY=0 0 0 0 0 0 0 0 0 0
-        CUT=  0 0  0 0  0 0  0 0  0 0  0 0  
-              0 0  0 0  0 0  0 0  0 0  0 0  
-            ! 0 0 0  0 0 0    0 0 0
-        H C MEMODE=2 PPMODE=0
-            | 0 0 0 0 0 0 0 0 0 0 
-        DECAY=0 0 0 0 0 0 0 0 0 0
-        CUT=  0 0  0 0  0 0  0 0  0 0  0 0  
-              0 0  0 0  0 0  0 0  0 0  0 0  
-            @ 0 0 0 0 0 0 0 0 0 0
-        DECAY=0 0 0 0 0 0 0 0 0 0
-        CUT=  0 0  0 0  0 0  0 0  0 0  0 0  
-              0 0  0 0  0 0  0 0  0 0  0 0  
-            ! 0 0 0  0 0 0    0 0 0
 TB      FORCES=force EVDISC=T RMAXH=rmaxh TRH=T RHO=T 3PV=pv
         MOL=mol GAMMA=F PAIR=pair SCALE={scale}
         UL={ul} IODEL={io} OVLP={ovlp} TBU={tbu} NOUAVG={nav} U1={u1}
 EWALD   TOL=ewtol NKDMX=1999 NKRMX=1999
 OPTIONS ASA[ADNF[0] NSPH[0] TWOC[0] CCOR[1]]"""
 
-  def write_control_file(self, filename, atoms):
+  def write_control_file(self, filename, atoms, nspec=2):
     """
     Generate control file for TBE code from template and Atoms object
     """
@@ -454,7 +277,8 @@ OPTIONS ASA[ADNF[0] NSPH[0] TWOC[0] CCOR[1]]"""
         'NBAS': len(atoms),
         'NK': self.nk,
         'PLAT_STR': plat_str,
-        'SITE_STR': site_str
+        'SITE_STR': site_str,
+        'NSPEC': nspec
         }    
     ctrl_file = open(filename, 'w')
     ctrl_file.write(self.template % dict)
